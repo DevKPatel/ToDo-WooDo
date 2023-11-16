4d43976b5fb669b76dade6c63f9e4c736005be61
DevKPatel <pateldev2816@gmail.com>
2023-11-16 17:37:42 +0530

    Creating CRUD funtionality with local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import React from "react";
+import TextTransition, { presets } from 'react-text-transition';
+import Main from './components/Main.jsx'
+const TEXTS = ['To-Do', 'WooDo'];
 
 function App() {
+  const [index, setIndex] = React.useState(0);
 
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      1500, // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
   return (
     <>
-        
+    <TextTransition springConfig={presets.default}  style={{
+      fontSize: '30px',
+      marginLeft: '20px',
+      marginTop: '20px',
+      fontWeight:'700',
+      fontFamily: 'Roboto'
+    }} >{TEXTS[index % TEXTS.length]}</TextTransition>
+    <Main />
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/Main.module.css'
+import Mainform from './Mainform'
+const Main = () => {
+  return (
+    <>
+    <div className={styles.body}>
+      <Mainform />
+    </div>
+    </>
+  )
+}
+
+export default Main
